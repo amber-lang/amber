b6284b556945136bface583c0235aca3d347f845
Ph0enixKM <pkaras.it@gmail.com>
2022-10-12 22:07:20 +0200

    fix: remove child process when invoking a function

--- src/cli/tests.rs
+++ src/cli/tests.rs
@@ -25,7 +25,7 @@ macro_rules! test_amber_err {
 
 #[test]
 fn hello_world() {
-    test_amber!("$echo Hello World$", "Hello World");
+    test_amber!("echo 'Hello World'", "Hello World");
 }
 
 #[test]
@@ -35,47 +35,47 @@ fn hello_world_error() {
 
 #[test]
 fn add() {
-    test_amber!("$echo {15 + 45}$", "60");
+    test_amber!("echo 15 + 45", "60");
 }
 
 #[test]
 fn mul() {
-    test_amber!("$echo {3 * 4}$", "12");
+    test_amber!("echo 3 * 4", "12");
 }
 
 #[test]
 fn div() {
-    test_amber!("$echo {21 / 3}$", "7");
+    test_amber!("echo 21 / 3", "7");
 }
 
 #[test]
 fn sub() {
-    test_amber!("$echo {21 - 7}$", "14");
+    test_amber!("echo 21 - 7", "14");
 }
 
 #[test]
 fn text() {
-    test_amber!("$echo {'Hello World'}$", "Hello World");
+    test_amber!("echo 'Hello World'", "Hello World");
 }
 
 #[test]
 fn bool() {
-    test_amber!("$echo {true}$", "1");
-    test_amber!("$echo {false}$", "0");
+    test_amber!("echo true", "1");
+    test_amber!("echo false", "0");
 }
 
 #[test]
 fn number() {
-    test_amber!("$echo {15}$", "15");
+    test_amber!("echo 15", "15");
 }
 
 #[test]
 fn variable() {
     let code = "
         let x = 42
-        $echo {x}$
+        echo x
         x = 21
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "42\n21");
 }
@@ -84,7 +84,7 @@ fn variable() {
 fn nested_string_interp() {
     let code = "
         let x = 'welcome {'to'} wonderful {'world'}'
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "welcome to wonderful world");
 }
@@ -95,7 +95,7 @@ fn complex_arithmetic() {
         let x = 21
         let y = 2
         let z = 3
-        $echo {x + y * z}$
+        echo x + y * z
     ";
     test_amber!(code, "27");
 }
@@ -107,7 +107,7 @@ fn very_complex_arithmetic() {
         let y = 2
         let z = 6
         let a = 4
-        $echo {x + y * z / a}$
+        echo x + y * z / a
     ";
     test_amber!(code, "24");
 }
@@ -119,7 +119,7 @@ fn paranthesis() {
         let y = 2
         let z = 6
         let a = 2
-        $echo {(x + y) * z / a}$
+        echo (x + y) * z / a
     ";
     test_amber!(code, "69");
 }
@@ -127,7 +127,7 @@ fn paranthesis() {
 #[test]
 fn command_interpolation() {
     let code = "
-        $echo {$echo {$echo Hello World$}$}$
+        echo $echo {$echo Hello World$}$
     ";
     test_amber!(code, "Hello World");
 }
@@ -154,7 +154,7 @@ fn compare_eq_texts() {
     let code = "
         let x = 'Hello World'
         let y = 'Hello World'
-        $echo {x == y}$
+        echo x == y
     ";
     test_amber!(code, "1");
 }
@@ -164,7 +164,7 @@ fn compare_eq_numbers() {
     let code = "
         let x = 42
         let y = 42
-        $echo {x == y}$
+        echo x == y
     ";
     test_amber!(code, "1");
 }
@@ -174,7 +174,7 @@ fn compare_neq_numbers() {
     let code = "
         let x = 42
         let y = 24
-        $echo {x != y}$
+        echo x != y
     ";
     test_amber!(code, "1");
 }
@@ -185,9 +185,9 @@ fn if_statements() {
         let x = 42
         let y = 24
         if x == y {
-            $echo {x}$
+            echo x
         } else {
-            $echo {y}$
+            echo y
         }
     ";
     test_amber!(code, "24");
@@ -199,10 +199,10 @@ fn if_statements_else() {
         let x = 42
         let y = 24
         if x == y {
-            $echo {x}$
+            echo x
         }
         else {
-            $echo {y}$
+            echo y
         }
     ";
     test_amber!(code, "24");
@@ -215,10 +215,10 @@ fn if_statement_chain() {
         let y = 24
         if {
             x == y {
-                $echo {x}$
+                echo x
             }
             else {
-                $echo {y}$
+                echo y
             }
         }
     ";
@@ -230,7 +230,7 @@ fn shorthand_add_text() {
     let code = "
         let x = 'Hello '
         x += 'World'
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "Hello World");
 }
@@ -240,7 +240,7 @@ fn shorthand_add() {
     let code = "
         let x = 16
         x += 48
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "64");
 }
@@ -250,7 +250,7 @@ fn shorthand_sub() {
     let code = "
         let x = 64
         x -= 16
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "48");
 }
@@ -260,7 +260,7 @@ fn shorthand_mul() {
     let code = "
         let x = 16
         x *= 4
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "64");
 }
@@ -270,7 +270,7 @@ fn shorthand_div() {
     let code = "
         let x = 21
         x /= 3
-        $echo {x}$
+        echo x
     ";
     test_amber!(code, "7");
 }
@@ -280,8 +280,8 @@ fn if_statements_singleline() {
     let code = "
         let x = 42
         let y = 24
-        if x == y => $echo {x}$
-        else => $echo {y}$
+        if x == y => echo x
+        else => echo y
     ";
     test_amber!(code, "24");
 }
@@ -291,8 +291,8 @@ fn if_statements_else_singleline() {
     let code = "
         let x = 42
         let y = 24
-        if x == y => $echo {x}$
-        else => $echo {y}$
+        if x == y => echo x
+        else => echo y
     ";
     test_amber!(code, "24");
 }
@@ -303,8 +303,8 @@ fn if_statement_chain_singleline() {
         let x = 42
         let y = 24
         if {
-            x == y => $echo {x}$
-            else => $echo {y}$
+            x == y => echo x
+            else => echo y
         }
     ";
     test_amber!(code, "24");
@@ -316,7 +316,7 @@ fn ternary_conditional_simple() {
         let a = 12 > 24
             then 42
             else 24
-        $echo {a}$
+        echo a
     ";
     test_amber!(code, "24");
 }
@@ -325,7 +325,7 @@ fn ternary_conditional_simple() {
 fn ternary_conditional_inline() {
     let code = "
         let a = 12 > 24 then 42 else 24
-        $echo {a}$
+        echo a
     ";
     test_amber!(code, "24");
 }
@@ -340,7 +340,7 @@ fn ternary_conditional_nested() {
             else (12 > 6
                 then 24
                 else 12)
-        $echo {a}$
+        echo a
     ";
     test_amber!(code, "24");
 }
@@ -367,7 +367,7 @@ fn infinite_loop() {
 fn modulo_operator() {
     let code = "
         let a = 10 % 3
-        $echo {a}$
+        echo a
     ";
     test_amber!(code, "1");
 }
@@ -377,7 +377,7 @@ fn modulo_shorthand() {
     let code = "
         let a = 10
         a %= 3
-        $echo {a}$
+        echo a
     ";
     test_amber!(code, "1");
 }
@@ -386,7 +386,7 @@ fn modulo_shorthand() {
 fn function() {
     let code = "
         fun test() {
-            $echo Hello World$
+            echo 'Hello World'
         }
         echo test()
     ";
@@ -397,19 +397,19 @@ fn function() {
 fn function_with_args() {
     let code = "
         fun test(a, b) {
-            $echo {a}$
-            $echo {b}$
+            echo a
+            echo b
         }
         echo test('Hello', 'World')
     ";
-    test_amber!(code, "Hello World");
+    test_amber!(code, "Hello\nWorld");
 }
 
 #[test]
 fn function_with_args_different_types() {
     let code = "
         fun test(a, b) {
-            $echo {a + b}$
+            echo a + b
         }
         echo test('Hello', 'World')
         echo test(11, 42)
@@ -421,7 +421,7 @@ fn function_with_args_different_types() {
 fn function_with_typed_args() {
     let code = "
         fun test(a: Num, b: Num) {
-            $echo {a + b}$
+            echo a + b
         }
         echo test(11, 42)
     ";
@@ -431,19 +431,19 @@ fn function_with_typed_args() {
 fn function_with_typed_different_args() {
     let code = "
         fun test(a: Num, b: Text) {
-            $echo {a}$
-            $echo {b}$
+            echo a
+            echo b
         }
         echo test(11, 'Hello')
     ";
-    test_amber!(code, "11 Hello");
+    test_amber!(code, "11\nHello");
 }
 
 #[test]
 fn function_with_typed_args_text() {
     let code = "
         fun test(a: Text, b: Text) {
-            $echo {a + b}$
+            echo a + b
         }
         echo test('Hello', 'World')
     ";
