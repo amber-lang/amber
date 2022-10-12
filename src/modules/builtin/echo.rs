71c5a8de486fe2f3306af274c6862a85617d778b
Ph0enixKM <pkaras.it@gmail.com>
2022-10-12 22:08:42 +0200

    lint fix

--- src/modules/builtin/echo.rs
+++ src/modules/builtin/echo.rs
@@ -30,7 +30,7 @@ impl TranslateModule for Echo {
         // If it's a function invocation we want to strip down the inner command
         // This way the newline characters in the stdout don't get lost
         if self.value.is_function_invocation() && value.starts_with('$') {
-            format!("{}", value.get(2..value.len() - 1).unwrap())
+            value.get(2..value.len() - 1).unwrap().to_string()
         } else {
             format!("echo {}", value)
         }
