da8c64f6a581c5c5257984a7a349dc1219cc34ff
b1ek <me@blek.codes>
2024-05-26 10:50:11 +1000

    strip variables in RDC

--- src/modules/rdc.rs
+++ src/modules/rdc.rs
@@ -19,11 +19,11 @@ pub fn scan_append_externs(strings: Vec<String>, meta: &mut TranslateMetadata) -
     // source: https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
     let bash_deps = vec![ "alias", "bind", "builtin", "caller", "command", "declare", "echo", "enable", "help", "let", "local", "logout", "mapfile", "printf", "read", "readarray", "source", "type", "typeset", "ulimit", "unalias" ];
     
-    let remove_prefix_whitespace = Regex::new(r"^ +").unwrap();
+    let remove_prefix_var = Regex::new(r"^(\w+=\S+( \w+=\S+)*|) +").unwrap();
     let remove_all_non_cmd = Regex::new(r" .*$").unwrap();
     
     for x in strings.iter() {
-        let parsed = remove_prefix_whitespace.replace(x, "").to_string();
+        let parsed = remove_prefix_var.replace(x, "").to_string();
         let parsed = remove_all_non_cmd.replace(&parsed, "").to_string();
         if x.len() != 0 && bash_deps.iter().find(|y| x.to_string() == y.to_string()).is_none() {
             meta.externs.push(parsed);
