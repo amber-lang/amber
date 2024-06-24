0e3acc3e3cb235d5f0cf6893f8dc4223658106fc
Federico Ceratto <federico@debian.org>
2024-06-24 23:55:06 +0200

    Add basic unit tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,7 +1,9 @@
 use crate::compiler::AmberCompiler;
+use difference::{Changeset, Difference};
 
 pub mod cli;
 pub mod stdlib;
+pub mod unit;
 pub mod validity;
 
 #[macro_export]
@@ -16,4 +18,64 @@ macro_rules! test_amber {
 
 pub fn compile_code<T: Into<String>>(code: T) -> String {
     AmberCompiler::new(code.into(), None).compile().unwrap().1
+}
+
+/// Compile code and compare it with the expected value.
+/// If any difference is found panic and show a friendly diff
+pub fn comp(source: &str, expected: &str) {
+    let mut exp = String::with_capacity(500);
+    let exp = {
+        // Dedent code blocks based on the first line after `r#"`
+        let mut lines = expected.lines();
+        lines.next();
+        let line = lines.next().unwrap_or("");
+        let indent = line.chars().take_while(|&c| c.is_whitespace()).count();
+        exp.push_str(&line[indent..]);
+        exp.push('\n');
+        for line in lines {
+            if line.len() >= indent {
+                exp.push_str(&line[indent..]);
+                exp.push('\n');
+            } else {
+                panic!("Incorrect indentation on line <<<{line}>>>.\nExpected {indent} characters")
+            }
+        }
+        exp.trim_end()
+    };
+
+    let compiled = compile_code(source);
+
+    let header = r#"#!/usr/bin/env bash
+# Written in [Amber](https://amber-lang.com/)
+"#;
+    if !compiled.starts_with(header) {
+        panic!("Unexpected header in generated code {compiled}")
+    }
+    // Ignore newlines before and after the generated code for ease of use
+    let compiled = compiled[65..]
+        .trim_end_matches("\n")
+        .trim_start_matches("\n");
+
+    if compiled == exp {
+        return;
+    };
+
+    let changeset = Changeset::new(&compiled, &exp, " ");
+
+    let mut out = String::new();
+    let mut cnt = 0;
+    for diff in changeset.diffs {
+        match diff {
+            Difference::Same(ref x) => out.push_str(x),
+            Difference::Add(ref x) => out.push_str(&format!("\x1b[32m『{}』\x1b[0m", x)),
+            Difference::Rem(ref x) => out.push_str(&format!("\x1b[31m『{}』\x1b[0m", x)),
+        }
+        cnt += 1;
+    }
+    println!("--- {} differences found ---", cnt);
+    println!("{}", out);
+    println!("--- generated code ---");
+    println!("{}", compiled);
+    println!("=======================================");
+    panic!("Unexpected difference");
 }
---
+++ src/tests/unit.rs
@@ -0,0 +1,70 @@
+use crate::tests::comp;
+
+#[test]
+fn unit_hello() {
+    comp(
+        r#"
+        echo "hello"
+        "#,
+        r#"
+        echo "hello"
+        "#,
+    );
+}
+
+#[test]
+fn unit_basic() {
+    comp(
+        r#"
+        import * from "std"
+
+        // Comment
+        let count = 1
+        let age = count > 1
+            then 18
+            else 28
+
+        age += 5
+        let name = "John"
+        let age = 18
+        echo "Hi, I'm {name}. I'm {age} years old."
+        "#,
+        r#"
+        __0_count=1
+        __1_age=$(if [ $(echo ${__0_count} '>' 1 | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//') != 0 ]; then echo 18; else echo 28; fi)
+        __1_age=$(echo ${__1_age} '+' 5 | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
+        __2_name="John"
+        __3_age=18
+        echo "Hi, I'm ${__2_name}. I'm ${__3_age} years old."
+        "#,
+    );
+}
+
+#[test]
+fn unit_basic_import() {
+    comp(
+        r#"
+        import * from "std"
+        main {
+            echo replace_once("hello world!", "world", "Amber")
+        }
+        echo "hi"
+        "#,
+        r#"
+        function replace_once__1_v0 {
+            local source=$1
+            local pattern=$2
+            local replacement=$3
+            __AMBER_VAL_0=$(echo "${source/${pattern}/${replacement}}");
+            __AS=$?;
+            __AF_replace_once1_v0="${__AMBER_VAL_0}";
+            return 0
+        }
+        
+            replace_once__1_v0 "hello world!" "world" "Amber";
+            __AF_replace_once1_v0__4="${__AF_replace_once1_v0}";
+            echo "${__AF_replace_once1_v0__4}"
+        echo "hi"
+        "#,
+    );
+}
