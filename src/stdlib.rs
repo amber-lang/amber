289d02d9e2d51e762f63ecaf3ee41d6c365cb0c8
Daniele Scasciafratte <mte90net@gmail.com>
2024-07-09 12:41:27 +0200

    fix(cargo): missing crate

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -3,7 +3,6 @@ pub const STDLIB: Dir = include_dir!("src/std");
 
 pub fn resolve<T: Into<String>>(path: T) -> Option<String> {
     let path = path.into();
-    let path = if path == "std" { "main".to_string() } else { path };
 
     if let Some(module) = STDLIB.get_file(path + ".ab") {
         let module = module.contents_utf8().unwrap().to_string();
