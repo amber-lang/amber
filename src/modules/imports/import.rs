4957174484318145324041a91b6c1cc5f9312d12
Daniele Scasciafratte <mte90net@gmail.com>
2024-07-09 14:59:31 +0200

    fix(resolve): remove std to search it

--- src/modules/imports/import.rs
+++ src/modules/imports/import.rs
@@ -71,7 +71,7 @@ impl Import {
 
     fn resolve_import(&mut self, meta: &ParserMetadata) -> Result<String, Failure> {
         if self.path.value.starts_with("std") {
-            match stdlib::resolve(&self.path.value) {
+            match stdlib::resolve(&self.path.value.replace("std/", "")) {
                 Some(v) => Ok(v),
                 None => error!(meta, self.token_path.clone() => {
                     message: format!("Std module does not exist: {}", self.path.value),
