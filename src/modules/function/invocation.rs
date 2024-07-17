986572bc900e80f15d693c6ee3be524e6098fce1
Daniele Scasciafratte <mte90net@gmail.com>
2024-07-17 10:37:27 +0200

    feat(review): done

--- src/modules/function/invocation.rs
+++ src/modules/function/invocation.rs
@@ -128,9 +128,9 @@ impl FunctionInvocation {
         if matches!(self.kind, Type::Array(_)) {
             format!("{quote}{dollar}{{{name}[@]}}{quote}")
         } else if matches!(self.kind, Type::Text) {
-            format!("{quote}{dollar}{{{name}}}{quote}")
+            format!("{quote}{dollar}{name}{quote}")
         } else {
-            format!("{dollar}{name}")
+            format!("{quote}{dollar}{{{name}}}{quote}")
         }
     }
 }
