02623b6813e8fab244f7ccbc6fcea266a8fe8ebd
MuhamedMagdi <m49dii@gmail.com>
2024-07-23 11:18:10 +0300

    feat: parse command moddifiers

--- src/modules/builtin/mv.rs
+++ src/modules/builtin/mv.rs
@@ -1,14 +1,18 @@
+use std::mem::swap;
+
 use heraclitus_compiler::prelude::*;
 use crate::modules::expression::expr::Expr;
 use crate::modules::condition::failed::Failed;
 use crate::translate::module::TranslateModule;
 use crate::docs::module::DocumentationModule;
 use crate::utils::{ParserMetadata, TranslateMetadata};
+use crate::modules::command::modifier::CommandModifier;
 
 #[derive(Debug, Clone)]
 pub struct Mv {
     source: Expr,
     destination: Expr,
+    modifier: CommandModifier,
     failed: Failed,
 }
 
@@ -20,15 +24,21 @@ impl SyntaxModule<ParserMetadata> for Mv {
             source: Expr::new(),
             destination: Expr::new(),
             failed: Failed::new(),
+            modifier: CommandModifier::new().parse_expr(),
         }
     }
 
     fn parse(&mut self, meta: &mut ParserMetadata) -> SyntaxResult {
-        token(meta, "mv")?;
-        syntax(meta, &mut self.source)?;
-        syntax(meta, &mut self.destination)?;
-        syntax(meta, &mut self.failed)?;
-        Ok(())
+        syntax(meta, &mut self.modifier)?;
+        self.modifier.use_modifiers(meta, |_this, meta| {
+            token(meta, "mv")?;
+            syntax(meta, &mut self.source)?;
+            syntax(meta, &mut self.destination)?;
+            match syntax(meta, &mut self.failed) {
+                Ok(_) => Ok(()),
+                Err(err) => Err(err)
+            }
+        })
     }
 }
 
@@ -36,7 +46,11 @@ impl TranslateModule for Mv {
     fn translate(&self, meta: &mut TranslateMetadata) -> String {
         let source = self.source.translate(meta);
         let destination = self.destination.translate(meta);
-        format!("mv {} {}", source, destination)
+        let failed = self.failed.translate(meta);
+        let mut is_silent = self.modifier.is_silent || meta.silenced;
+        swap(&mut is_silent, &mut meta.silenced);
+        let silent = meta.gen_silent();
+        format!("mv {source} {destination}{silent}\n{failed}").trim_end().to_string()
     }
 }
 
