e81b481a1024fc90f6293602603b9877f96859e0
Daniele Scasciafratte <mte90net@gmail.com>
2024-07-23 14:08:37 +0200

    Update src/tests/stdlib.rs
    Co-authored-by: Phoenix Himself <pkaras.it@gmail.com>

--- src/tests/stdlib.rs
+++ src/tests/stdlib.rs
@@ -29,7 +29,7 @@ fn stdlib_test(input: &str) {
         }
 
         if _is_output {
-            if ! output.is_empty() {
+            if !output.is_empty() {
                 output.push_str("\n");
             }
             output.push_str(&line.replace("//", "").trim());
