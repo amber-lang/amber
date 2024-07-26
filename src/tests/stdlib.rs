cf346d92069e930b1584fafc4e1755106b56c1fe
Daniele Scasciafratte <mte90net@gmail.com>
2024-07-26 11:08:30 +0200

    feat(review): variable

--- src/tests/stdlib.rs
+++ src/tests/stdlib.rs
@@ -17,19 +17,19 @@ fn stdlib_test(input: &str) {
     let code = fs::read_to_string(input)
     .expect(&format!("Failed to open {input} test file"));
 
-    let mut _is_output = false;
+    let mut is_output = false;
     let mut output = "".to_owned();
     for line in code.lines() {
         if line.starts_with("// Output") {
-            _is_output = true;
+            is_output = true;
             continue;
         } else if line.is_empty() && _is_output {
-            _is_output = false;
+            is_output = false;
             break;
         }
 
-        if _is_output {
-            if !output.is_empty() {
+        if is_output {
+            if ! output.is_empty() {
                 output.push_str("\n");
             }
             output.push_str(&line.replace("//", "").trim());
