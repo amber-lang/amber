8cbadf45b0b612041023dd8801044d7dba085194
Luci <lucidashygirl@proton.me>
2024-08-04 04:19:05 -0400

    denested handle_compile and handle_docs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ pub struct Cli {
     eval: Option<String>,
 
     /// Generate docs
-    /// (OUTPUT is dir instead, default: `docs/`) 
+    /// (OUTPUT is dir instead, default: `docs/`)
     #[arg(long)]
     docs: bool,
 
@@ -55,59 +55,72 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_compile(cli: Cli) -> Result<(), Box<dyn Error>> {
+    let valid_input: String;
     if let Some(input) = cli.input.clone() {
-        let input = String::from(input.to_string_lossy().trim());
-        let code = {
-            if input == "-" {
-                let mut buf = String::new();
-                match stdin().read_to_string(&mut buf) {
-                    Ok(_) => buf,
-                    Err(err) => handle_err(err),
-                }
-            } else {
-                match fs::read_to_string(&input) {
-                    Ok(code) => code,
-                    Err(err) => handle_err(err),
-                }
+        valid_input = String::from(input.to_string_lossy().trim());
+    } else {
+        return Ok(());
+    }
+
+    let code = {
+        if valid_input == "-" {
+            let mut buf = String::new();
+            match stdin().read_to_string(&mut buf) {
+                Ok(_) => buf,
+                Err(err) => handle_err(err),
             }
-        };
-        match AmberCompiler::new(code, Some(input), cli.clone()).compile() {
-            Ok((messages, code)) => {
-                messages.iter().for_each(|m| m.show());
-                // Save to the output file
-                if let Some(output) = cli.output {
-                    let output = String::from(output.to_string_lossy());
-                    if output == "--silent" {
-                        return Ok(());
-                    }
-                    if output == "-" {
-                        print!("{code}");
-                        return Ok(());
-                    }
-                    match fs::File::create(&output) {
-                        Ok(mut file) => {
-                            write!(file, "{}", code).unwrap();
-                            set_file_permission(&file, output);
-                        }
-                        Err(err) => {
-                            Message::new_err_msg(err.to_string()).show();
-                            std::process::exit(1);
-                        }
-                    }
-                }
-                // Execute the code
-                else {
-                    (!messages.is_empty()).then(render_dash);
-                    let exit_status = AmberCompiler::execute(code, &[])?;
-                    std::process::exit(exit_status.code().unwrap_or(1));
-                }
-            }
-            Err(err) => {
-                err.show();
-                std::process::exit(1);
+        } else {
+            match fs::read_to_string(&valid_input) {
+                Ok(code) => code,
+                Err(err) => handle_err(err),
             }
         }
+    };
+
+    let valid_messages;
+    let valid_code: String;
+    match AmberCompiler::new(code, Some(valid_input), cli.clone()).compile() {
+        Ok((messages, code)) => {
+            valid_messages = messages;
+            valid_code = code;
+        }
+        Err(err) => {
+            err.show();
+            std::process::exit(1);
+        }
     }
+    valid_messages.iter().for_each(|m| m.show());
+    // Save to the output file
+    let valid_output: String;
+    if let Some(output) = cli.output {
+        valid_output = String::from(output.to_string_lossy());
+    } else {
+        // Execute the code
+        (!valid_messages.is_empty()).then(render_dash);
+        let exit_status = AmberCompiler::execute(valid_code, &[])?;
+        std::process::exit(exit_status.code().unwrap_or(1));
+    }
+
+    if valid_output == "--silent" {
+        return Ok(());
+    }
+
+    if valid_output == "-" {
+        print!("{valid_code}");
+        return Ok(());
+    }
+
+    match fs::File::create(&valid_output) {
+        Ok(mut file) => {
+            write!(file, "{}", valid_code).unwrap();
+            set_file_permission(&file, valid_output);
+        }
+        Err(err) => {
+            Message::new_err_msg(err.to_string()).show();
+            std::process::exit(1);
+        }
+    }
+
     Ok(())
 }
 
@@ -128,31 +141,37 @@ fn handle_eval(code: String, cli: Cli) -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_docs(cli: Cli) -> Result<(), Box<dyn Error>> {
+    let valid_input: String;
     if let Some(ref input) = cli.input {
-        let input = String::from(input.to_string_lossy());
-        let output = {
-            let out = cli.output.clone().unwrap_or_else(|| PathBuf::from("docs"));
-            String::from(out.to_string_lossy())
-        };
-        match fs::read_to_string(&input) {
-            Ok(code) => match AmberCompiler::new(code, Some(input), cli).generate_docs(output) {
-                Ok(_) => Ok(()),
-                Err(err) => {
-                    err.show();
-                    std::process::exit(1);
-                }
-            },
-            Err(err) => {
-                Message::new_err_msg(err.to_string()).show();
-                std::process::exit(1);
-            }
-        }
+        valid_input = String::from(input.to_string_lossy());
     } else {
         Message::new_err_msg(
             "You need to provide a path to an entry file to generate the documentation",
         )
         .show();
         std::process::exit(1);
+    }
+
+    let output = {
+        let out = cli.output.clone().unwrap_or_else(|| PathBuf::from("docs"));
+        String::from(out.to_string_lossy())
+    };
+
+    let valid_code: String;
+    match fs::read_to_string(&valid_input) {
+        Ok(code) => valid_code = code,
+        Err(err) => {
+            Message::new_err_msg(err.to_string()).show();
+            std::process::exit(1);
+        }
+    }
+
+    match AmberCompiler::new(valid_code, Some(valid_input), cli).generate_docs(output) {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            err.show();
+            std::process::exit(1);
+        }
     }
 }
 
