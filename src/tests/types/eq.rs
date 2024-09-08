50d212027e6b4ae6fcf12ebfa7feaf1438ff92cc
b1ek <me@blek.codes>
2024-09-08 15:40:40 +1000

    test: cover some more type equals

--- src/tests/types/eq.rs
+++ src/tests/types/eq.rs
@@ -1,8 +1,11 @@
 use crate::modules::types::Type;
 
 #[test]
-fn two_normal_types() {
-    assert_eq!(Type::Bool, Type::Bool);
+fn normal_types_eq() {
+    let types = vec![Type::Null, Type::Text, Type::Bool, Type::Num, Type::Generic];
+    for typ in types {
+        assert_eq!(typ, typ, "{typ} and {typ} must be equal!");
+    }
 }
 
 #[test]
@@ -13,4 +16,14 @@ fn two_different_normal_types() {
 #[test]
 fn normal_and_failable_type() {
     assert_ne!(Type::Failable(Box::new(Type::Text)), Type::Text, "Text? and Text must not be equal!")
+}
+
+#[test]
+fn array_and_normal_type() {
+    assert_ne!(Type::Array(Box::new(Type::Bool)), Type::Bool);
+}
+
+#[test]
+fn array_and_array_of_failables() {
+    assert_ne!(Type::Array(Box::new(Type::Bool)), Type::Array(Box::new(Type::Failable(Box::new(Type::Bool)))));
 }
