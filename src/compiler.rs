4c1a478cc6a1b4e6a96720b1223219b085b48fe3
Daniele Scasciafratte <mte90net@gmail.com>
2024-09-19 10:34:39 +0200

    fix(review): removed code

--- src/compiler.rs
+++ src/compiler.rs
@@ -224,11 +224,7 @@ impl AmberCompiler {
             let dir_path = {
                 let file_dir = dep_path.strip_prefix(&base_dir).unwrap();
                 let parent = file_dir.parent().unwrap().display();
-                if !output.is_empty() {
-                    output.clone()
-                } else {
-                    format!("{}/{output}/{}", base_dir.to_string_lossy(), parent)
-                }
+                format!("{}/{output}/{}", base_dir.to_string_lossy(), parent)
             };
             if let Err(err) = fs::create_dir_all(dir_path.clone()) {
                 Message::new_err_msg(format!(
