3cdaaed80e84e5388f4d375baae80dac955b229d
b1ek <me@blek.codes>
2024-11-01 08:42:07 +1000

    fix type

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -25,7 +25,7 @@ fn bash_error_exit_code() -> Result<(), Box<dyn std::error::Error>> {
 
     // Changes locale to default to prevent locale-specific error messages.
     cmd.env("LC_ALL", "C")
-        .arg("--no-processor")
+        .arg("--no-proc")
         .arg("*")
         .arg(file.path());
 
