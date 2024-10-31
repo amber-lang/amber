93c21efd6643bee81f7f5a90eb8cbf78e2ae64ea
b1ek <me@blek.codes>
2024-10-31 18:12:49 +1000

    rewrite postprocessor test to be simpler but require all default postprocessors to be present

--- src/tests/postprocessor.rs
+++ src/tests/postprocessor.rs
@@ -1,31 +1,26 @@
-use std::{
-    env::current_dir, fs::{self, Permissions}, os::unix::fs::PermissionsExt
-};
 
 use crate::compiler::postprocess::PostProcessor;
 
-fn create_fake_binary(processor: &PostProcessor) {
-    let body = if cfg!(unix) {
-        "#!/usr/bin/env bash\nexit 0"
-    } else {
-        panic!("this test is not available for non-unix platforms")
-    };
-
-    fs::write(&processor.bin, body).expect("Couldn't write fake script");
-    fs::set_permissions(&processor.bin, Permissions::from_mode(0o755))
-        .expect("Couldn't set perms for fake script");
-}
+use super::compile_code;
 
 #[test]
-fn all_exist() {
-    let processor = PostProcessor::new(
-        "test",
-        current_dir().unwrap().join("test.sh"),
-    );
-
-    create_fake_binary(&processor);
+fn default_ok() {
+    let default = PostProcessor::get_default();
     
-    let ok = processor.is_available();
-    fs::remove_file("test.sh").expect("Couldn't remove fake script");
-    assert!(ok, "Postprocessor is unavailable but it should be! It is likely an issue with the environment");
+    let hello = "echo \"Hello world!\"";
+    let hello = compile_code(hello);
+
+    let mut unavailable = vec![];
+
+    for processor in &default {
+        if ! processor.is_available() {
+            unavailable.push(processor.name.clone());
+        }
+    }
+
+    assert!(unavailable.len() == 0, "These commands have to be in $PATH for this test to pass: {}", unavailable.join(", "));
+
+    for processor in default {
+        assert!(processor.execute(hello.clone()).is_ok(), "Postprocessor {} couldn't process hello world", processor.name)
+    }
 }
