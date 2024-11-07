0c96ae4f3d53f96664245507d75c64fa54d5f983
Huw Walters <huw.walters@gmail.com>
2024-11-07 19:37:50 +0000

    Make binary add join lines and text array.

--- src/modules/expression/binop/add.rs
+++ src/modules/expression/binop/add.rs
@@ -67,10 +67,11 @@ impl TranslateModule for Add {
                 let quote = meta.gen_quote();
                 let id = meta.gen_array_id();
                 let name = format!("__AMBER_ARRAY_ADD_{id}");
-                meta.stmt_queue.push_back(format!("{name}=({left} {right})"));
+                meta.stmt_queue.push_back(format!("{name}=({left})"));
                 if let Some(append) = self.left.append_let(meta, &name, false) {
                     meta.stmt_queue.push_back(append);
                 }
+                meta.stmt_queue.push_back(format!("{name}+=({right})"));
                 if let Some(append) = self.right.append_let(meta, &name, false) {
                     meta.stmt_queue.push_back(append);
                 }
