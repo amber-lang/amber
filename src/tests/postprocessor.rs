b4c81cd3d884ea6ae9bb040124647d1ed161baca
Huw Walters <huw.walters@gmail.com>
2024-11-23 09:18:54 +0000

    Make postprocessors optional in integration tests.

--- src/tests/postprocessor.rs
+++ src/tests/postprocessor.rs
@@ -5,26 +5,17 @@ use super::compile_code;
 
 #[test]
 fn default_ok() {
-    let default = PostProcessor::get_default();
-    
     let hello = "echo \"Hello world!\"";
     let hello = compile_code(hello);
 
-    let mut unavailable = vec![];
-
-    for processor in &default {
-        if ! processor.is_available() {
-            unavailable.push(processor.name.clone());
+    let processors = PostProcessor::get_default();
+    for processor in processors {
+        if processor.is_available() {
+            let res = processor.execute(hello.clone());
+            assert!(res.is_ok(), "Postprocessor {} couldn't process hello world", processor.name);
+            let res = res.unwrap();
+            let (stdout, _) = eval_bash(res);
+            assert_eq!(stdout, "Hello world!");
         }
-    }
-
-    assert!(unavailable.is_empty(), "These commands have to be in $PATH for this test to pass: {}", unavailable.join(", "));
-
-    for processor in default {
-        let res = processor.execute(hello.clone());
-        assert!(res.is_ok(), "Postprocessor {} couldn't process hello world", processor.name);
-        let res = res.unwrap();
-        let (stdout, _) = eval_bash(res);
-        assert_eq!(stdout, "Hello world!");
     }
 }
