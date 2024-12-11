5b1f4e7633efa1016531a2eac22be58003474e10
Phoenix Himself <pkaras.it@gmail.com>
2024-12-11 18:31:05 +0100

    fix: function naming

--- src/tests/erroring.rs
+++ src/tests/erroring.rs
@@ -2,8 +2,8 @@ use super::script_test;
 use super::TestOutcomeTarget;
 use test_generator::test_resources;
 
-/// Autoload the Amber test files in stdlib, match the output in the comment
+/// Autoload the Amber test files in erroring, match the output in the comment
 #[test_resources("src/tests/erroring/*.ab")]
-fn test_stdlib(input: &str) {
+fn test_erroring(input: &str) {
     script_test(input, TestOutcomeTarget::Failure);
 }
