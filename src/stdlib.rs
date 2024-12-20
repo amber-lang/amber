d40638ce486341cd0ff42f96aad2f8402bbd3293
KrosFire <hubik080@gmail.com>
2024-12-20 23:01:07 +0100

    Added std lib files into installer

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,12 +1,28 @@
-use include_dir::{include_dir, Dir};
-pub const STDLIB: Dir = include_dir!("src/std");
+use std::{env, fs, path::PathBuf};
+
+#[cfg(not(debug_assertions))]
+fn get_install_dir() -> PathBuf {
+    let path = env::var("STD_PATH").expect("STD_PATH not set");
+
+    PathBuf::from(path)
+}
+
+#[cfg(debug_assertions)]
+fn get_install_dir() -> PathBuf {
+    let path = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+
+    PathBuf::from(path).join("resources/std")
+}
 
 pub fn resolve<T: Into<String>>(path: T) -> Option<String> {
-    let path = path.into();
+    let path = get_install_dir().join(path.into() + ".ab");
 
-    if let Some(module) = STDLIB.get_file(path + ".ab") {
-        let module = module.contents_utf8().unwrap().to_string();
-        Some(module)
+    if !path.is_file() {
+        return None;
+    }
+
+    if let Ok(contents) = fs::read_to_string(path) {
+        Some(contents)
     } else {
         None
     }
