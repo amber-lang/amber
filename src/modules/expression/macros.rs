17c8854eee3da6f78f52edacb7f0921ee1a15924
Huw Walters <huw.walters@gmail.com>
2024-12-26 09:51:14 +0000

    Import regex crate.

--- src/modules/expression/macros.rs
+++ src/modules/expression/macros.rs
@@ -270,3 +270,11 @@ macro_rules! document_expression {
         }
     }
 }
+
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
