74b8330867f9d87e014a054e0fe0b0e288d518f1
KrosFire <hubik080@gmail.com>
2025-01-11 22:02:51 +0100

    Improved scripts and brought back snap

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,19 +1,5 @@
 use std::{env, fs, path::PathBuf};
 
-#[cfg(not(debug_assertions))]
-fn get_install_dir() -> PathBuf {
-    let exec_path = env::current_exe().expect("Could not fetch executable file path.");
-
-    PathBuf::from(exec_path.read_link().unwrap_or(exec_path).parent().unwrap()).join("std")
-}
-
-#[cfg(debug_assertions)]
-fn get_install_dir() -> PathBuf {
-    let path = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-
-    PathBuf::from(path).join("std")
-}
-
 pub fn resolve<T: Into<String>>(path: T) -> Option<String> {
     let path = get_install_dir().join(path.into() + ".ab");
 
@@ -26,4 +12,18 @@ pub fn resolve<T: Into<String>>(path: T) -> Option<String> {
     } else {
         None
     }
+}
+
+#[cfg(not(debug_assertions))]
+fn get_install_dir() -> PathBuf {
+    let exec_path = env::current_exe().expect("Could not fetch executable file path.");
+
+    PathBuf::from(exec_path.read_link().unwrap_or(exec_path).parent().unwrap()).join("std")
+}
+
+#[cfg(debug_assertions)]
+fn get_install_dir() -> PathBuf {
+    let path = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+
+    PathBuf::from(path).join("std")
 }
