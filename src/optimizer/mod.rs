2818bde4f9d1b6443777f08db8626a91e0f6d047
Phoenix Himself <pkaras.it@gmail.com>
2025-05-20 17:42:20 +0000

    feat: add scaffolding

---
+++ src/optimizer/mod.rs
@@ -0,0 +1,7 @@
+use crate::modules::prelude::*;
+
+mod var;
+
+pub fn optimize_fragments(ast: FragmentKind) -> FragmentKind {
+    
+}
---
+++ src/optimizer/var.rs
@@ -0,0 +1,7 @@
+use crate::modules::prelude::*;
+
+pub fn remove_unused_variables(ast: FragmentKind) -> FragmentKind {
+    match ast{
+        
+    }
+}
