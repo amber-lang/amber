0b53ae7bdbc1ea643d93e5f1005fac7c9b9e470b
Daniele Scasciafratte <mte90net@gmail.com>
2025-10-02 10:35:36 +0200

    Update error messages for failed command handling

--- src/modules/condition/failed.rs
+++ src/modules/condition/failed.rs
@@ -67,10 +67,10 @@ impl SyntaxModule<ParserMetadata> for Failed {
                             return error_pos!(meta, pos, format!("Failed function call '{fun_name}' must be followed by a 'then', 'succeeded' or 'failed' block, statement or operator '?'"))
                         }
                         (None, Some(pos)) => {
-                            return error_pos!(meta, pos, format!("Failed command must be followed by a 'succeeded' or 'failed' block, statement or operator '?'"))
+                            return error_pos!(meta, pos, format!("Failed command must be followed by a 'then', 'succeeded' or 'failed' block, statement or operator '?'"))
                         }
                         _ => {
-                            return error!(meta, tok, format!("Failed expression must be followed by a 'succeeded' or 'failed' block, statement or operator '?'"))
+                            return error!(meta, tok, format!("Failed expression must be followed by a 'then', 'succeeded' or 'failed' block, statement or operator '?'"))
                         }
                     }
                 }
