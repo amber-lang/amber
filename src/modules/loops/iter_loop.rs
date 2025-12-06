a8af8ec5721600d3dd8afbc435051ef9aba56c3f
Phoenix Himself <pkaras.it@gmail.com>
2025-12-06 21:36:56 +0100

    feat: add range loop when translating

--- src/modules/loops/iter_loop.rs
+++ src/modules/loops/iter_loop.rs
@@ -12,6 +12,7 @@ use crate::utils::metadata::{ParserMetadata, TranslateMetadata};
 use crate::modules::block::Block;
 use crate::{fragments, raw_fragment};
 use crate::modules::prelude::*;
+use crate::modules::expression::binop::range::Range;
 
 #[derive(Debug, Clone)]
 pub struct IterLoop {
@@ -65,6 +66,14 @@ impl SyntaxModule<ParserMetadata> for IterLoop {
 impl TranslateModule for IterLoop {
     fn translate(&self, meta: &mut TranslateMetadata) -> FragmentKind {
         let iter_path = self.translate_path(meta);
+        
+        // Optimize range loops
+        if iter_path.is_none() {
+            if let Some(ExprType::Range(range)) = &self.iter_expr.value {
+                return self.translate_range_loop(range, meta);
+            }
+        }
+
         let iter_name = raw_fragment!("{}", get_variable_name(&self.iter_name, self.iter_global_id));
 
         let for_loop_prefix = match iter_path.is_some() {
@@ -142,6 +151,114 @@ impl IterLoop {
         } else {
             None
         }
+    }
+
+    fn translate_dynamic_range_loop(
+        &self, range: &Range,
+        meta: &mut TranslateMetadata,
+        from_var: FragmentKind,
+        to_var: FragmentKind
+    ) -> FragmentKind {
+        let iter_name = raw_fragment!("{}", get_variable_name(&self.iter_name, self.iter_global_id));
+
+        // Calculate direction
+        // dir = from < to ? 1 : -1
+        let dir_val = fragments!("$(( ", from_var.clone(), " < ", to_var.clone(), " ? 1 : -1 ))");
+        let dir_stmt = VarStmtFragment::new("dir", Type::Int, dir_val);
+        let dir_var = meta.push_ephemeral_variable(dir_stmt).with_quotes(false).to_frag();
+
+        // Operator
+        let op = raw_fragment!("{}", if range.neq { "<" } else { "<=" });
+
+        // Define index handling if needed
+        let (index_init, index_update) = match (self.iter_index.as_ref(), self.iter_index_global_id) {
+            (Some(index), Some(global_id)) => {
+                let idx_var = get_variable_name(index, Some(global_id));
+                (raw_fragment!(", {idx_var}=0"), raw_fragment!(", {idx_var}++"))
+            },
+            _ => (FragmentKind::Empty, FragmentKind::Empty)
+        };
+
+        let body = self.block.translate(meta);
+
+        let init = fragments!(iter_name.clone(), "=", from_var, index_init);
+        // We do a trick here by multiplying by dir so that we can use the same comparison operator
+        // iter_name * dir_var < to_var * dir_var
+        let cond = fragments!(iter_name.clone(), " * ", dir_var.clone(), " ", op, " ", to_var, " * ", dir_var.clone());
+        let update = fragments!(iter_name, "+=", dir_var, index_update);
+
+        let stmt = fragments!(
+            "for (( ", init, "; ", cond, "; ", update, " )); do\n",
+            body,
+            "\ndone"
+        );
+        
+        stmt
+    }
+    
+    fn translate_range_loop(
+        &self, 
+        range: &Range, 
+        meta: &mut TranslateMetadata
+    ) -> FragmentKind {
+        // Optimization: Try to resolve range bounds at compile time
+        if let (Some(from_val), Some(to_val)) = (range.from.get_integer_value(), range.to.get_integer_value()) {
+            return self.translate_static_range_loop(range, meta, from_val, to_val);
+        }
+        
+        // Dynamic range
+        let from = range.from.translate(meta);
+        let from_var = meta.push_ephemeral_variable(VarStmtFragment::new("range_start", Type::Int, from))
+            .with_quotes(false).to_frag();
+
+        let to = range.to.translate(meta);
+        let to_var = meta.push_ephemeral_variable(VarStmtFragment::new("range_end", Type::Int, to))
+            .with_quotes(false).to_frag();
+
+        self.translate_dynamic_range_loop(range, meta, from_var, to_var)
+    }
+
+    fn translate_static_range_loop(
+        &self,
+        range: &Range,
+        meta: &mut TranslateMetadata,
+        from_val: isize,
+        to_val: isize
+    ) -> FragmentKind {
+        if range.neq && from_val == to_val {
+            return FragmentKind::Empty;
+        }
+        
+        let iter_name = RawFragment::from(get_variable_name(&self.iter_name, self.iter_global_id)).to_frag();
+        // Define index iterator variable if used
+        let (index_init, index_update) = match (self.iter_index.as_ref(), self.iter_index_global_id) {
+            (Some(index), Some(global_id)) => {
+                let idx_var = get_variable_name(index, Some(global_id));
+                (raw_fragment!(", {idx_var}=0"), raw_fragment!(", {idx_var}++"))
+            },
+            _ => (FragmentKind::Empty, FragmentKind::Empty)
+        };
+
+        let body = self.block.translate(meta);
+        let (op, step) = if from_val <= to_val {(
+            raw_fragment!("{}", if range.neq { "<" } else { "<=" }),
+            raw_fragment!("++")
+        )} else {(
+            raw_fragment!("{}", if range.neq { ">" } else { ">=" }),
+            raw_fragment!("--")
+        )};
+        
+        let init = fragments!(iter_name.clone(), "=", raw_fragment!("{from_val}"), index_init);
+        let cond = fragments!(iter_name.clone(), " ", op, " ", raw_fragment!("{to_val}"));
+        let update = fragments!(iter_name, step, index_update);
+
+        let loop_frag = fragments!(
+            "for (( ", init, "; ", cond, "; ", update, " )); do\n",
+            body,
+            "\ndone"
+        );
+        
+        loop_frag
     }
 }
 
