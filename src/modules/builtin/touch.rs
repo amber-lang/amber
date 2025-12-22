33e1018b08fa4311eda4aba1f4a9c91b256f6453
Daniele Scasciafratte <mte90net@gmail.com>
2025-12-22 16:33:13 +0100

    fix(amber): wip

--- src/modules/builtin/touch.rs
+++ src/modules/builtin/touch.rs
@@ -21,14 +21,14 @@ impl SyntaxModule<ParserMetadata> for Touch {
         token(meta, "touch")?;
 
         if token(meta, "(").is_ok() {
-            syntax(meta, &mut *self.value)?;
+            syntax(meta, &mut self.value)?;
             token(meta, ")")?;
         } else {
             let tok = meta.get_token_at(position);
             let warning = Message::new_warn_at_token(meta, tok)
             .message("Calling a builtin without parentheses is deprecated");
             meta.add_message(warning);
-            syntax(meta, &mut *self.value)?;
+            syntax(meta, &mut self.value)?;
         }
         Ok(())
     }
