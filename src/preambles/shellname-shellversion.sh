4c0d1d597fb923f0c14aa65e90a4114fdbce6790
Tirito <easycraft250@gmail.com>
2026-04-21 15:44:34 +0200

    Update shellname-shellversion.sh

--- src/preambles/shellname-shellversion.sh
+++ src/preambles/shellname-shellversion.sh
@@ -3,8 +3,8 @@ if [ -n "$ZSH_VERSION" ]; then
     IFS='.' read -A EXEC_SHELL_VERSION <<< "$ZSH_VERSION"
 elif [ -n "$KSH_VERSION" ]; then
     EXEC_SHELL="ksh"
-    __exec_shell_version="${KSH_VERSION:-}"
-    IFS='.' read -a EXEC_SHELL_VERSION <<< "${__exec_shell_version%% *}"
+    __exec_shell_version="${KSH_VERSION##* }"
+    IFS='-' read -A EXEC_SHELL_VERSION <<< "${__exec_shell_version%% *}"
 else
     EXEC_SHELL="bash"
     EXEC_SHELL_VERSION=("${BASH_VERSINFO[0]}" "${BASH_VERSINFO[1]}" "${BASH_VERSINFO[2]}")
