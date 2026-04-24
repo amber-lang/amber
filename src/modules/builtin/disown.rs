c27fe04497a5fb1b7c15fa98e6b2a225c823e2e0
Hubert Jabłoński <hubik080@gmail.com>
2026-04-24 20:55:42 +0200

    Added pids to disown

--- src/modules/builtin/disown.rs
+++ src/modules/builtin/disown.rs
@@ -1,5 +1,8 @@
-use crate::fragments;
+use crate::{fragments, raw_fragment};
+use crate::modules::expression::expr::Expr;
 use crate::modules::prelude::*;
+use crate::modules::types::{Type, Typed};
+use crate::translate::fragments::var_stmt::VarStmtFragment;
 use crate::utils::ParserMetadata;
 use amber_meta::AutoKeyword;
 use heraclitus_compiler::prelude::*;
@@ -8,32 +11,84 @@ use heraclitus_compiler::syntax_name;
 #[derive(Clone, Debug, AutoKeyword)]
 #[keyword = "disown"]
 #[kind = "builtin_stmt"]
-pub struct Disown {}
+pub struct Disown {
+    pids: Option<Expr>,
+}
 
 impl SyntaxModule<ParserMetadata> for Disown {
     syntax_name!("disown");
 
     fn new() -> Self {
-        Disown {}
+        Disown { pids: None }
     }
 
     fn parse(&mut self, meta: &mut ParserMetadata) -> SyntaxResult {
         token(meta, "disown")?;
         token(meta, "(")?;
-        token(meta, ")")?;
+
+        if token(meta, ")").is_err() {
+            let mut pids = Expr::new();
+            syntax(meta, &mut pids)?;
+            self.pids = Some(pids);
+            token(meta, ")")?;
+        }
+
         Ok(())
     }
 }
 
 impl TypeCheckModule for Disown {
-    fn typecheck(&mut self, _meta: &mut ParserMetadata) -> SyntaxResult {
+    fn typecheck(&mut self, meta: &mut ParserMetadata) -> SyntaxResult {
+        if let Some(ref mut pids) = self.pids {
+            pids.typecheck(meta)?;
+
+            let pids_type = pids.get_type();
+            let expected_array_type = Type::array_of(Type::Int);
+            if pids_type != Type::Int && !pids_type.is_allowed_in(&expected_array_type) {
+                let position = pids.get_position();
+                return error_pos!(meta, position => {
+                    message: "Builtin function `disown` can only be used with values of type Int or [Int]",
+                    comment: format!("Given type: {}, expected type: {} or {}", pids_type, Type::Int, expected_array_type)
+                });
+            }
+        }
+
         Ok(())
     }
 }
 
 impl TranslateModule for Disown {
-    fn translate(&self, _meta: &mut TranslateMetadata) -> FragmentKind {
-        fragments!("disown")
+    fn translate(&self, meta: &mut TranslateMetadata) -> FragmentKind {
+        let Some(pids) = &self.pids else {
+            return fragments!("disown 2>/dev/null || true");
+        };
+
+        let pids_type = pids.get_type();
+        let pids_var_stmt =
+            VarStmtFragment::new("__disown_pids", pids_type.clone(), pids.translate(meta))
+                .with_global_id(meta.gen_value_id())
+                .with_optimization_when_unused(false);
+        let pids_var = meta.push_ephemeral_variable(pids_var_stmt.clone());
+        let pids_var_name = pids_var.get_name();
+
+        let iter_value = if pids_type.is_array() {
+            format!("\"${{{pids_var_name}[@]}}\"")
+        } else {
+            format!("\"${{{pids_var_name}}}\"")
+        };
+
+        let pid_var = format!("__AMBER_PID_{}", meta.gen_value_id());
+
+        BlockFragment::new(vec![
+            raw_fragment!("for {pid_var} in {iter_value}; do"),
+            BlockFragment::new(
+                vec![
+                    raw_fragment!("disown ${pid_var} 2>/dev/null || true")
+                ],
+                true,
+            ).to_frag(),
+            raw_fragment!("done"),
+        ], false).to_frag()
     }
 }
 
