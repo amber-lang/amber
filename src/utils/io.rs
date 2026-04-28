ea893a9db9eee61146e795b9eaa66c755befa807
siba-dev <commits@siba.dev>
2026-04-28 23:33:10 +0200

    Added explicit support for symbolic links to find_amber_files

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 /// Adds all amber files (.ab extension) from the directory to the vector. The search is recursive.
 ///
@@ -7,14 +7,14 @@ use std::path::PathBuf;
 /// * `files` Vector to add results to
 pub fn find_amber_files(dir: &PathBuf, files: &mut Vec<PathBuf>) -> std::io::Result<()> {
     if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
+        for entry in WalkDir::new(dir).follow_links(true) {
             let entry = entry?;
             let path = entry.path();
-            if path.is_dir() {
-                find_amber_files(&path, files)?;
-            } else if let Some(ext) = path.extension() {
-                if ext == "ab" {
-                    files.push(path);
+            if path.is_file() {
+                if let Some(ext) = path.extension() {
+                    if ext == "ab" {
+                        files.push(path.to_path_buf());
+                    }
                 }
             }
         }
