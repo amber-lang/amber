2b6732a74fec0885641182e449c8193528bc2993
Hubert Jabłoński <hubik080@gmail.com>
2026-05-07 16:46:05 +0200

    Added one more test

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -536,3 +536,27 @@ fn test_cli_eval() {
     .success()
     .stdout(predicate::str::starts_with("Hello world"));
 }
+
+#[test]
+fn test_cli_param_injection() {
+    let temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    let amber_code = r#"
+        main (args) {
+            echo(args[1])
+        }
+        "#;
+
+    std::fs::write(temp_file.path(), amber_code).expect("Failed to write test file");
+
+    let mut cmd = Command::new(amber_bin());
+    cmd.args([
+        "run",
+        temp_file.path().to_str().unwrap(),
+        "$HOME"
+    ])
+    .assert()
+    .success()
+    .stdout(predicate::str::starts_with("$HOME"));
+
+    let _ = temp_file.close();
+}
