64d68081d57aa9951cf28fe87db32a74aa7e7b69
rondDev <contact@rond.cc>
2026-08-07 19:18:44 +0200

    fix: call from_args with shebang

--- src/main.rs
+++ src/main.rs
@@ -420,7 +420,8 @@ fn create_output_dir(command: &BuildCommand, file: &Path) -> PathBuf {
 fn build_file(command: &BuildCommand, target: &Option<ShellType>, input: PathBuf, output: PathBuf) {
     let options = CompilerOptions::from_args(&command.no_proc, command.minify, false, None)
         .with_target(*target)
-        .with_env_vars();
+        .with_env_vars()
+        .with_shebang(command.shebang.clone());
 
     let (code, _) = compile_input(input, options);
     write_output(output, code);
