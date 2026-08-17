897b7529c9e37f006e541d80092090a4412042ce
Daniele Scasciafratte <mte90net@gmail.com>
2026-08-17 12:11:08 +0200

    Update src/modules/expression/binop/and.rs
    Co-authored-by: Tirito <99983969+Tirito6626@users.noreply.github.com>

--- src/modules/expression/binop/and.rs
+++ src/modules/expression/binop/and.rs
@@ -116,12 +116,7 @@ impl TranslateModule for And {
             Some(value) if !self.has_side_effects() => {
                 RawFragment::from((if value { "1" } else { "0" }).to_string()).to_frag()
             }
-            Some(_) => {
-                let left = self.left.translate(meta);
-                let right = self.right.translate(meta);
-                ConditionFragment::new(left, ComparisonOperator::And, right).to_frag()
-            }
-            None => {
+            Some(_) | None => {
                 let left = self.left.translate(meta);
                 let right = self.right.translate(meta);
                 ConditionFragment::new(left, ComparisonOperator::And, right).to_frag()
