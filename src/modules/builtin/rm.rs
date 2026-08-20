b6b5cd680217540ed5fd03fb4c1a5f998d928836
Daniele Scasciafratte <mte90net@gmail.com>
2026-08-20 14:39:39 +0200

    Render text-adjacent array interpolations with [*] (SC2145)

--- src/modules/builtin/rm.rs
+++ src/modules/builtin/rm.rs
@@ -122,11 +122,14 @@ impl TranslateModule for Rm {
                     recursive_expr.get_name()
                 )
             )]);
-            recursive_expr.to_frag()
+            let recursive_name = recursive_expr.get_name();
+            raw_fragment!("${{{}:+\"${{{}}}\"}}", recursive_name, recursive_name)
         } else {
             let recursive_var_stmt = VarStmtFragment::new("__rm", Type::Bool, raw_fragment!(""))
                 .with_global_id(recursive_id);
-            meta.push_ephemeral_variable(recursive_var_stmt).to_frag()
+            let var_expr = meta.push_ephemeral_variable(recursive_var_stmt);
+            let name = var_expr.get_name();
+            raw_fragment!("${{{}:+\"${{{}}}\"}}", name, name)
         };
 
         let force_id = meta.gen_value_id();
@@ -145,11 +148,14 @@ impl TranslateModule for Rm {
                     force_expr.get_name()
                 )
             )]);
-            force_expr.to_frag()
+            let force_name = force_expr.get_name();
+            raw_fragment!("${{{}:+\"${{{}}}\"}}", force_name, force_name)
         } else {
             let recursive_var_stmt = VarStmtFragment::new("__rm", Type::Bool, raw_fragment!(""))
                 .with_global_id(force_id);
-            meta.push_ephemeral_variable(recursive_var_stmt).to_frag()
+            let var_expr = meta.push_ephemeral_variable(recursive_var_stmt);
+            let name = var_expr.get_name();
+            raw_fragment!("${{{}:+\"${{{}}}\"}}", name, name)
         };
 
         let silent = meta.with_silenced(self.modifier.is_silent || meta.silenced, |meta| {
